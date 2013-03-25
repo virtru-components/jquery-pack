7e22f7a7182f76e6f607c2f5854b33ce8ea0566f
Reuven V. Gonzales <reuven@virtru.com>
2013-03-25 19:46:55 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Require all of the plugins
+require('jquery-trigger-native');
+require('jquery-firstChild');
+require('jquery-waiter');
+
+var $ = require('jquery');
+
+//Export jquery
+module.exports = $;
